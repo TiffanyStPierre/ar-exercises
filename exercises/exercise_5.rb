757bc82094928c4697a006a157cff2bba8fb9b13
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-12-11 09:41:56 -0700

    complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts Store.sum('annual_revenue')
+
+average_annual_revenue = Store.sum('annual_revenue') / Store.count
+
+puts average_annual_revenue
+
+rich_stores = Store.where("annual_revenue < ?", 1000000)
+
+puts rich_stores.count
