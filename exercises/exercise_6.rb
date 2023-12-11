04d609b88390afd163f2b10df8caee9a0be9f7ee
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-12-11 10:02:22 -0700

    complete exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,9 +11,9 @@ puts "----------"
 # Your code goes here ...
 @store1.employees.create(first_name: "Gina", last_name: "Hobbs", hourly_rate: 60)
 @store1.employees.create(first_name: "Brie", last_name: "Smith", hourly_rate: 40)
-@store1.employees.create(first_name: "Bob", last_name: "Jones", hourly_rate: 20)
+@store1.employees.create(first_name: "Bob", last_name: "Jones", hourly_rate: 80)
 
 @store2.employees.create(first_name: "Jessie", last_name: "Yates", hourly_rate: 60)
 @store2.employees.create(first_name: "Richard", last_name: "Hanson", hourly_rate: 70)
 @store2.employees.create(first_name: "Brenda", last_name: "Bates", hourly_rate: 55)
-@store2.employees.create(first_name: "Mike", last_name: "White", hourly_rate: 35)
+@store2.employees.create(first_name: "Mike", last_name: "White", hourly_rate: 45)
